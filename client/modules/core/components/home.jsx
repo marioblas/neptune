9f25b8f10aa87db638a78ba2f5033e2ee3626920
Mario Blas <marioblasgc@gmail.com>
2016-07-01 23:13:20 +0100

    Update core components style

--- client/modules/core/components/home.jsx
+++ client/modules/core/components/home.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import styles from './style.scss';
+import styles from './home.scss';
 
 const Home = () => (
   <div className={styles.home}>
-    <h1 className={styles.greeting}>Welcome to Neptune!</h1>
+    <h1 className={styles.greeting}>Welcome to Neptune</h1>
   </div>
 );
 
