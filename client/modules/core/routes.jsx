265aacc720b8be1d1f2676be555e0cbc6ba13234
Mario Blas <marioblasgc@gmail.com>
2016-09-09 13:13:13 +0100

    Fix home view

--- client/modules/core/routes.jsx
+++ client/modules/core/routes.jsx
@@ -3,7 +3,7 @@ import { mount } from 'react-mounter';
 
 import MainLayout from './components/layout';
 
-import Home from './components/home';
+import Home from './components/home.jsx';
 
 export default function (injectDeps, { FlowRouter }) {
   const MainLayoutCtx = injectDeps(MainLayout);
