abe95ef485f4a2256db76038d3b54255bdc96b9d
Mario Blas <marioblasgc@gmail.com>
2016-10-15 02:04:55 +0100

    Add initial browser policy config

--- server/main.js
+++ server/main.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import publications from './publications';
 import methods from './methods';
+import configureBrowserPolicy from './configs/browser_policy';
 import configureEmail from './configs/email';
 import configureChalk from './configs/chalk';
 import addInitialData from './configs/initial_data';
@@ -9,6 +10,7 @@ import addFakeData from './configs/fake_data';
 publications();
 methods();
 
+configureBrowserPolicy();
 configureEmail();
 configureChalk();
 addInitialData();
