3df77221c7a15948841705c70570e23530628536
Mario Blas <marioblasgc@gmail.com>
2016-12-21 23:39:30 +0000

    Add initial data with some roles and users

--- server/configs/initial_data.js
+++ server/configs/initial_data.js
@@ -1,3 +1,30 @@
 // Add initial data here...
+import { Meteor } from 'meteor/meteor';
+import { Roles } from 'meteor/alanning:roles';
+import { Accounts } from 'meteor/accounts-base';
 
-export default function () {}
+export default function () {
+  // Roles
+  if (Roles.getAllRoles().fetch().length === 0) {
+    const roles = ['root', 'admin'];
+    roles.forEach((role) => {
+      Roles.createRole(role);
+    });
+  }
+
+  // Users
+  const users = [
+    { username: 'root', email: 'root@example.com', password: 'root', roles: ['root'] },
+    { username: 'admin', email: 'admin@example.com', password: 'admin', roles: ['admin'] },
+  ];
+
+  users.forEach(({ username, email, password, roles }) => {
+    const userExists = Accounts.findUserByEmail(email);
+
+    if (!userExists) {
+      const userId = Accounts.createUser({ username, email, password });
+      Meteor.users.update(userId, { $set: { 'emails.0.verified': true } });
+      Roles.addUsersToRoles(userId, roles);
+    }
+  });
+}
