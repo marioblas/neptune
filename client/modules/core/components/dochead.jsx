ee30ca54d9848e5bfc81678e94d827851ab3e03e
Mario Blas <marioblasgc@gmail.com>
2017-04-14 01:40:01 +0100

    Add html lang

--- client/modules/core/components/dochead.jsx
+++ client/modules/core/components/dochead.jsx
@@ -6,6 +6,7 @@ const DocHead = () => (
     defaultTitle="Neptune"
     titleTemplate="%s | Neptune"
   >
+    <html lang="en" />
     <meta name="description" content="A Neptune application" />
     {/* Color the status bar on mobile devices (you can use any valid CSS color) */}
     <meta name="theme-color" content="" />
