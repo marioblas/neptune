05035a43086ad2470597a64df41b6b235c43fbaf
Mario Blas <marioblasgc@gmail.com>
2017-06-23 20:10:53 +0100

    Storybook: add some Button stories

---
+++ client/ui/.stories/Button.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { MemoryRouter } from 'react-router';
+
+import Button from '../Button';
+
+storiesOf('Button', module)
+  .add('default', () => (
+    <Button onClick={action('clicked')}>Default</Button>
+  ))
+  .add('primary', () => (
+    <Button primary onClick={action('clicked')}>Primary</Button>
+  ))
+  .add('as anchor', () => (
+    <Button
+      href="https://github.com/marioblas/neptune"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Nepune on Github
+    </Button>
+  ))
+  .addDecorator(story => (
+    <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
+  ))
+  .add('as link', () => (
+    <Button to="/">Go to root</Button>
+  ));
--- client/ui/.stories/index.js
+++ client/ui/.stories/index.js
@@ -1,14 +1,1 @@
-import React from 'react';
-
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-
-import Button from '../Button';
-
-storiesOf('Button', module)
-  .add('default', () => (
-    <Button onClick={action('clicked')}>Hello world</Button>
-  ))
-  .add('primary', () => (
-    <Button primary onClick={action('clicked')}>Hello world</Button>
-  ))
+import './Button';
